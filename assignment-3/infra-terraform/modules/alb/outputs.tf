3ff6da1221f620e17cff065cc8bb4cac27350a98
Vivek Raj <vr32288@gmail.com>
2025-09-20 11:41:50 +0530

    Create outputs.tf

---
+++ assignment-3/infra-terraform/modules/alb/outputs.tf
@@ -0,0 +1,34 @@
+output "alb_arn" {
+  description = "ARN of the Application Load Balancer"
+  value       = aws_lb.main.arn
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.main.dns_name
+}
+
+output "alb_zone_id" {
+  description = "Zone ID of the Application Load Balancer"
+  value       = aws_lb.main.zone_id
+}
+
+output "target_group_arn" {
+  description = "ARN of the target group"
+  value       = aws_lb_target_group.main.arn
+}
+
+output "security_group_id" {
+  description = "Security group ID for ALB"
+  value       = aws_security_group.alb.id
+}
+
+output "http_listener_arn" {
+  description = "ARN of the HTTP listener"
+  value       = aws_lb_listener.http.arn
+}
+
+output "https_listener_arn" {
+  description = "ARN of the HTTPS listener"
+  value       = var.enable_https ? aws_lb_listener.https[0].arn : null
+}
