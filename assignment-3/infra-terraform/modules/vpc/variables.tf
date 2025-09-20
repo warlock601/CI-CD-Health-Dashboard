420a57727b528bde3b7b8e0c5b10b44b2a56087e
Vivek Raj <vr32288@gmail.com>
2025-09-20 11:54:25 +0530

    Create variables.tf for VPC

---
+++ assignment-3/infra-terraform/modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
